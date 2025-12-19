56638cd173369046c7a67d1d43175500952f6582
javaijafoi <57443592+javaijafoi@users.noreply.github.com>
2025-12-19 00:10:35 -0300

    Fix immer imports to use named export

--- lib/hooks/useLazyState/__tests__/useLazyState.test.tsx
+++ lib/hooks/useLazyState/__tests__/useLazyState.test.tsx
@@ -1,5 +1,5 @@
 import { act, renderHook } from "@testing-library/react";
-import produce from "immer";
+import { produce } from "immer";
 import { useLazyState } from "../useLazyState";
 
 describe("useLazyState", () => {
